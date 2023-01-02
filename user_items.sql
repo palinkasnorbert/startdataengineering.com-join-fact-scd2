6b71895eecb798346d4b599bfde925968ad713fe
Norbert Palinkas <norbert@palinkasnorbert.hu>
2023-01-02 12:31:10 +0100

    edits

--- user_items.sql
+++ user_items.sql
@@ -4,5 +4,5 @@ SELECT ip.item_purchased_id,
         ud.zipcode,
         ip.purchased_datetime
 FROM items_purchased ip
-        JOIN user_dim ud ON ip.user_id = ud.user_id
+        JOIN user_dim AS ud ON ip.user_id = ud.user_id
         AND ip.purchased_datetime BETWEEN ud.row_effective_datetime AND ud.row_expiration_datetime
