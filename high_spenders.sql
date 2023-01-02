318a1ab21e006a1c643976fd1ace87c9df0e0202
Norbert Palinkas <norbert@palinkasnorbert.hu>
2023-01-02 12:23:23 +0100

    user_items.sql created

--- high_spenders.sql
+++ high_spenders.sql
@@ -1,4 +1,4 @@
 SELECT  user_id
 FROM items_purchased
 GROUP BY  user_id
-HAVING SUM(item_cost) > 1000
+HAVING SUM(item_cost) > 1000;
---
+++ user_items.sql
@@ -0,0 +1,8 @@
+SELECT ip.item_purchased_id,
+        ip.user_id,
+        ip.item_cost,
+        ud.zipcode,
+        ip.purchased_datetime
+FROM items_purchased ip
+        JOIN user_dim ud ON ip.user_id = ud.user_id
+        AND ip.purchased_datetime BETWEEN ud.row_effective_datetime AND ud.row_expiration_datetime
